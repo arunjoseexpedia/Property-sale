bc080854cc81f8e0b2321f3ae88df24892bdbd19
Arun <arun.konkoth@ust.com>
2025-09-18 14:59:43 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/arunjoseexpedia/",
+  base: '/Property-sale/',
   plugins: [react()],
 })
